1522370ae88b55ba5d59aa99453180efbe2b59ff
EstherRC2 <esthercastilla1996@gmail.com>
2024-06-19 17:05:34 +0200

    t1,4

--- Bloque1. Conceptos-básicos/01.03-Conversión-de-Variables.py
+++ Bloque1. Conceptos-básicos/01.03-Conversión-de-Variables.py
@@ -28,3 +28,14 @@ print(f"Suma: {int(b) + float(c)}")
 print(type(int(b) + float(c)))
 
 print(f"Número: {d}  <-- No se puede convertir a Float por contener una a")
+
+
+# La conversión es muy importante cuando se trabajan datos de diferente
+# tipo, las conversiones se realizan añadiendo el tipo en el que quieres
+# convertir la variable ejemplos:
+# tupla = tuple(lista)      convierte de lista a tupla
+# lista = list(tupla)       convierte de tupla a lista
+# numero = int(n)           convierte en numero entero
+# float(n)                  convierte en numero decimal
+# cadena = str(n)           convierte en texto
+# booleano = bool(n)        convierte en bool (verdadero o falso)
